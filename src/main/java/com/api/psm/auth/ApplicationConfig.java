58732f7785f0f882d97270e51316b7bec94377b6
achmad <achmad@192.168.56.1>
2021-05-22 15:29:37 +0700

    update

--- src/main/java/com/api/psm/auth/ApplicationConfig.java
+++ src/main/java/com/api/psm/auth/ApplicationConfig.java
@@ -36,7 +36,7 @@ public class ApplicationConfig {
 	}
 
 	private ApiInfo getApiInfo() {
-		String appName = "MT Auth API";
+		String appName = "eblo-api-psm-auth";
 		
 		return new ApiInfo(appName, appName, "1.0", "",
 				new Contact(appName, appName, "EMAIL"), "LICENSE", "LICENSE URL",
