52f24e532d5a4c5deeb15727054a4a52aeb7ae3b
achmad <achmad@192.168.56.1>
2021-06-05 15:01:15 +0700

    update

--- eblo_api_psm_auth.sql
+++ eblo_api_psm_auth.sql
@@ -29,8 +29,6 @@ CREATE TABLE `tb_auth` (
   `tba_update_date` datetime DEFAULT NULL,
   `tba_update_id` int(11) DEFAULT NULL,
   `tba_email` varchar(255) DEFAULT NULL,
-  `tba_member_id` varchar(255) DEFAULT NULL,
-  `tba_id_login` varchar(255) DEFAULT NULL,
   `tba_password` varchar(32) DEFAULT NULL,
   `tba_status` varchar(20) DEFAULT NULL,
   `tba_token_salt` varchar(36) DEFAULT NULL,
@@ -44,7 +42,7 @@ CREATE TABLE `tb_auth` (
 
 LOCK TABLES `tb_auth` WRITE;
 /*!40000 ALTER TABLE `tb_auth` DISABLE KEYS */;
-INSERT INTO `tb_auth` VALUES (1,'2019-09-03 15:43:36',0,'2020-07-30 10:20:33',0,'achmad.amri@gmail.com','999991','999991','202cb962ac59075b964b07152d234b70','active','jnsr8q8t7dzgqv8qqmkfsffc5mqdn5fec4af');
+INSERT INTO `tb_auth` VALUES (1,'2019-09-03 15:43:36',0,'2020-07-30 10:20:33',0,'achmad.amri@gmail.com','202cb962ac59075b964b07152d234b70','active','jnsr8q8t7dzgqv8qqmkfsffc5mqdn5fec4af');
 /*!40000 ALTER TABLE `tb_auth` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -57,4 +55,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-06-02 15:57:20
+-- Dump completed on 2021-06-05 15:00:46
