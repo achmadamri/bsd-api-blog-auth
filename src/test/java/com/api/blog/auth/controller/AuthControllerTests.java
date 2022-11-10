9df8a2ed6a737b15edd55c88ec801a33c047df4f
amri <achmad.amri@gmail.com>
2022-11-10 16:54:48 +0700

    update

--- src/test/java/com/api/blog/auth/controller/AuthControllerTests.java
+++ src/test/java/com/api/blog/auth/controller/AuthControllerTests.java
@@ -1,22 +1,58 @@
 package com.api.blog.auth.controller;
 
-//@RunWith(MockitoJUnitRunner.class)
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.api.blog.auth.db.entity.TbAuth;
+import com.api.blog.auth.db.repository.TbAuthRepository;
+import com.api.blog.auth.model.auth.PostGenerateRequestModel;
+import com.api.blog.auth.util.TokenUtil;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class AuthControllerTests {
+
+	@Autowired
+	private AuthController authController;
 	
-//	@Mock
-//	TbAuthRepository tbAuthRepository;
-//	
-//	@MockBean
-//	AuthService authService;
-//	
-//	@Test
-//	public void postAdd() throws Exception {
-//		AuthAddRequestModel requestModel = new AuthAddRequestModel();
-//		requestModel.setEmail("achmad.amri@gmail.com");
-//		requestModel.setToken("0123456789");
-//		requestModel.setRequestDate("2019-11-15T16:58:26.170092");
-//		requestModel.setRequestId("abcdefghij");
-//		
-//		when(authService.postAdd(requestModel));
-//	}
+	@MockBean
+	private TbAuthRepository tbAuthRepository;
+	
+	@MockBean
+	private TokenUtil tokenUtil;
+	
+	@Test
+	public void postGenerate() throws Exception {
+		PostGenerateRequestModel requestModel = new PostGenerateRequestModel();
+		requestModel.setRequestDate("2022-11-07T21:57:35.839000");
+		requestModel.setRequestId("1pl53s0ier");
+		requestModel.setTbaEmail("admin@mail.com");
+		requestModel.setTbaIdLogin("admin@mail.com");
+		requestModel.setTbaPassword("123");
+		
+		TbAuth tbAuth = new TbAuth();
+		tbAuth.setTbaEmail("admin@mail.com");
+		Optional<TbAuth> optTbAuthByEmail = Optional.of(tbAuth);
+		when(tbAuthRepository.findOne(any())).thenReturn(optTbAuthByEmail);
+		
+		when(tokenUtil.generate(any(), any())).thenReturn("token dummy");
+		
+		when(tbAuthRepository.save(any())).thenReturn(null);
+		
+		when(tokenUtil.claims(any())).thenReturn(null);
+		
+		ResponseEntity<?> responseEntity = (ResponseEntity<?>) authController.postGenerate(requestModel);
+		assertEquals(200, responseEntity.getStatusCodeValue());
+	}
 }
